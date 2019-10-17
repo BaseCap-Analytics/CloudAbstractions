51ecbe448542811afb7d003d8e0a46ebd88067ca
Jonathan Miller <jonathan.miller@basecapanalytics.com>
2019-10-17 09:42:41 -0700

    Adding SendGrid abstraction and implementation

---
+++ src/Abstractions/IEmailSender.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BaseCap.CloudAbstractions.Abstractions
+{
+    /// <summary>
+    /// Constract for interacting with an email provider to send templated, automated emails
+    /// </summary>
+    public interface IEmailSender
+    {
+        /// <summary>
+        /// Sends a pre-defined template email to a set of users
+        /// </summary>
+        /// <param name="toAddresses">The name/address combination of recipients</param>
+        /// <param name="templateId">The ID of the template to send</param>
+        /// <param name="templateData">Any object data to be JSON serialized as variables to the template</param>
+        /// <param name="logger">The logger to use for error reporting</param>
+        /// <param name="token">A cancellation token to cancel the request</param>
+        /// <returns>Returns true if the request was successful; otherwise, returns false</returns>
+        Task<bool> SendTemplateEmailAsync<T>(
+            IEnumerable<(string email, string name)> toAddresses,
+            string templateId,
+            T? templateData,
+            ILogger logger,
+            CancellationToken token = default(CancellationToken)) where T: class;
+    }
+}
---
+++ src/Implementations/SendGrid/SendGridEmailSender.cs
@@ -0,0 +1,79 @@
+using BaseCap.CloudAbstractions.Abstractions;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using SendGrid.Helpers.Reliability;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BaseCap.CloudAbstractions.Implementations.SendGrid
+{
+    /// <summary>
+    /// A connection to SendGrid to send automated emails
+    /// </summary>
+    public class SendGridEmailSender : IEmailSender
+    {
+        private readonly SendGridClient _client;
+        private readonly EmailAddress _fromAddress;
+
+        /// <summary>
+        /// Creates a new SendGridEmailSender
+        /// </summary>
+        /// <param name="apiKey">The SendGrid API key</param>
+        /// <param name="fromAddress">The email address that the emails should come from</param>
+        /// <param name="fromName">The display name of the from address</param>
+        public SendGridEmailSender(string apiKey, string fromAddress, string fromName)
+        {
+            _client = new SendGridClient(new SendGridClientOptions()
+            {
+                ApiKey = apiKey,
+                ReliabilitySettings = new ReliabilitySettings(
+                    3,
+                    TimeSpan.FromSeconds(5),
+                    TimeSpan.FromMinutes(3),
+                    TimeSpan.FromSeconds(30)),
+            });
+            _fromAddress = new EmailAddress(fromAddress, fromName);
+        }
+
+        /// <inheritdoc />
+        public async Task<bool> SendTemplateEmailAsync<T>(
+            IEnumerable<(string email, string name)> toAddresses,
+            string templateId,
+            T? templateData,
+            ILogger logger,
+            CancellationToken token = default(CancellationToken)) where T : class
+        {
+            SendGridMessage msg = new SendGridMessage();
+            msg.SetFrom(_fromAddress);
+            msg.AddTos(toAddresses.Select(t => new EmailAddress(t.email, t.name)).ToList());
+            msg.SetTemplateId(templateId);
+
+            if (templateData != null)
+            {
+                msg.SetTemplateData(templateData);
+            }
+
+            Response resp = await _client.SendEmailAsync(msg, token).ConfigureAwait(false);
+            if ((resp.StatusCode == System.Net.HttpStatusCode.Accepted) ||
+                (resp.StatusCode == System.Net.HttpStatusCode.OK))
+            {
+                return true;
+            }
+            else
+            {
+                logger.LogEvent(
+                    "SendEmailFailed",
+                    new Dictionary<string, string>()
+                    {
+                        ["TemplateId"] = templateId,
+                        ["RecipientCount"] = toAddresses.Count().ToString(),
+                        ["ErrorCode"] = resp.StatusCode.ToString(),
+                    });
+                return false;
+            }
+        }
+    }
+}
